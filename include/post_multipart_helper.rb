d10c35b046ce0124485fe2c81586adef14775f22
kaspernj <k@spernj.org>
2014-07-10 12:47:09 +0200

    Extracted code out into add_headers and add_value to make it more maintenable.

--- include/post_multipart_helper.rb
+++ include/post_multipart_helper.rb
@@ -13,31 +13,8 @@ class Http2::PostMultipartHelper
   def generate_raw(phash)
     Tempfile.open("http2_post_multipart_tmp_#{@boundary}") do |praw|
       phash.each do |key, val|
-        praw << "--#{@boundary}#{@nl}"
-
-        if val.is_a?(Tempfile) && val.respond_to?(:original_filename)
-          parse_temp_file(key, val, praw)
-        elsif val.is_a?(Hash) && val[:filename]
-          parse_file(key, val, praw)
-        else
-          praw << "Content-Disposition: form-data; name=\"#{key}\";#{@nl}"
-          praw << "Content-Length: #{val.to_s.bytesize}#{@nl}"
-        end
-
-        praw << "Content-Type: text/plain#{@nl}"
-        praw << @nl
-
-        if val.class.name.to_s == "StringIO"
-          praw << val.read
-        elsif val.is_a?(Hash) && val[:content]
-          praw << val[:content].to_s
-        elsif val.is_a?(Hash) && val[:fpath]
-          read_file(val[:fpath], praw)
-        else
-          praw << val.to_s
-        end
-
-        praw << @nl
+        add_headers(praw, key, val)
+        add_body(praw, val)
       end
 
       praw << "--#{@boundary}--"
@@ -73,5 +50,35 @@ class Http2::PostMultipartHelper
     else
       raise "Could not figure out where to get content from."
     end
+  end
+
+  def add_headers(praw, key, val)
+    praw << "--#{@boundary}#{@nl}"
+
+    if val.is_a?(Tempfile) && val.respond_to?(:original_filename)
+      parse_temp_file(key, val, praw)
+    elsif val.is_a?(Hash) && val[:filename]
+      parse_file(key, val, praw)
+    else
+      praw << "Content-Disposition: form-data; name=\"#{key}\";#{@nl}"
+      praw << "Content-Length: #{val.to_s.bytesize}#{@nl}"
+    end
+
+    praw << "Content-Type: text/plain#{@nl}"
+    praw << @nl
+  end
+
+  def add_body(praw, val)
+    if val.class.name.to_s == "StringIO"
+      praw << val.read
+    elsif val.is_a?(Hash) && val[:content]
+      praw << val[:content].to_s
+    elsif val.is_a?(Hash) && val[:fpath]
+      read_file(val[:fpath], praw)
+    else
+      praw << val.to_s
+    end
+
+    praw << @nl
   end
 end
