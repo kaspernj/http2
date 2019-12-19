a1a7c72e16d6d9f75e3a807f07404a987fc06237
kaspernj <kaspernj@gmail.com>
2019-12-19 11:51:33 +0100

    Fixed offence

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require "helpers"
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
